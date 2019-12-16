de6d321bc47209d1aca75b985a782368f67c9745
文约前端 <zhangshuopeng@bonc.com.cn>
2019-12-16 15:38:56 +0800

    ds

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    测试表达式打算
+    测试表达式打算大萨达所
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
